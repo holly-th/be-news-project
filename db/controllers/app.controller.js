3ccdc78352e60e27dc06de59ff031902c76e9b2a
holly-th <holly.thomas135@gmail.com>
2023-02-20 14:22:20 +0000

    happy path tests passing, error 500 add to error handling file but not tested for

--- db/controllers/app.controller.js
+++ db/controllers/app.controller.js
@@ -3,5 +3,4 @@ exports.getTopics = (req, res, next) => {
   fetchTopics().then((results) => {
     res.status(200).send(results);
   });
-  //.catch(next);
 };
--- db/error-handling.js
+++ db/error-handling.js
@@ -1,7 +1,3 @@
 exports.serverError = (err, req, res, next) => {
-  if (err) {
-    console.log(err);
-  } else {
-    res.status(500).send({ message: `ERROR: server error` });
-  }
+  res.status(500).send({ message: `ERROR: server error` });
 };
