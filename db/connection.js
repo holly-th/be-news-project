b1d2f3dfe8161bd150add75e817ded5435fdf7cc
holly-th <holly.thomas135@gmail.com>
2023-02-24 17:00:02 +0000

    hosting

--- db/connection.js
+++ db/connection.js
@@ -5,16 +5,15 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
 const config =
   ENV === "production"
     ? {
-        connectionStr: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         max: 2,
       }
     : {};
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 module.exports = new Pool(config);
